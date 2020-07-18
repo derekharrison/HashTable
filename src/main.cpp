83a71ff5f37480337a1ca82e1ca60860a4fe67c8
Derek <harrison.derek.w@gmail.com>
2020-07-18 11:49:23 +0200

    removed saved keys

--- src/main.cpp
+++ src/main.cpp
@@ -16,18 +16,16 @@ int main(int argc, char* argv[])
 
     /* Inserting random data in hash table. Note: hash table only admits unique keys */
     int size_table = 150;
-    int* saved_keys = new int[size_table];
     srand(time(NULL));
     for(int i = 0; i < size_table; ++i) {
         int key = rand() % size_table;
-        saved_keys[i] = key;
         Data data = {key * 3.14159};
         Node node = {key, data, };
         htable.insert_node(node);
     }
 
     htable.print_data_table();
-    Data data = htable.get_data_node(saved_keys[size_table-1]);
+    Data data = htable.get_data_node(98);
     std::cout << "data node: " << data.number << std::endl;
     std::cout << "size table: " << htable.get_size_table() << std::endl;
     htable.print_keys_table();
