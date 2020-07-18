c8375648dbef750f53fd51afc6e04fb35e495f90
Derek <harrison.derek.w@gmail.com>
2020-07-18 12:58:04 +0200

    add call to delete_node()

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,7 @@ int main(int argc, char* argv[])
     std::cout << "data node: " << data.number << std::endl;
     std::cout << "size table: " << htable.get_size_table() << std::endl;
     htable.print_keys_table();
+    htable.delete_node(key);
     std::cout << "delete table" << std::endl;
     htable.delete_table();
     htable.print_keys_table();
