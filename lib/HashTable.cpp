d4f025a53461ec44e51c5cdef515e20002450a65
Derek <harrison.derek.w@gmail.com>
2020-07-20 23:42:29 +0200

    add HashTable:: to get_node_ptr in check_key_taken

--- lib/HashTable.cpp
+++ lib/HashTable.cpp
@@ -109,7 +109,7 @@ Data HashTable::get_data_node(int key) {
 
 bool HashTable::check_key_taken(int key) {
     bool key_taken = false;
-    Node* node_ptr = get_node_ptr(key);
+    Node* node_ptr = HashTable::get_node_ptr(key);
     /* Case: Node with matching key found */
     if(node_ptr != NULL) {
         key_taken = true;
